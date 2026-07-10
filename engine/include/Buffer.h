9f00e35fa124ff8b41c5db6056b03d9b01e21ffa
robert.culver <robertculver90@gmail.com>
2026-07-10 20:42:19 +0100

    prospective buffer changes

--- engine/include/Buffer.h
+++ engine/include/Buffer.h
@@ -18,3 +18,57 @@ template <class T> struct Buffer{
 private:
 	bool bufInitialised = false;
 };
+
+
+/*
+#pragma once
+#include <cstddef>
+#include <algorithm>
+#include <vector>
+#include <limits>
+
+using uint  = unsigned int;
+using uchar = unsigned char;
+
+template<typename T>
+class Buffer {
+public:
+    Buffer() = default;
+    Buffer(uint width, uint height);
+
+    // Rule of 5
+    ~Buffer() = default;
+    Buffer(const Buffer&) = default;
+    Buffer(Buffer&&) noexcept = default;
+    Buffer& operator=(const Buffer&) = default;
+    Buffer& operator=(Buffer&&) noexcept = default;
+
+    void init(T default_value = T{});
+    void reset(T value = T{});
+
+    T getMax() const;
+    T getMin() const;
+
+    // Accessors
+    T* data() { return m_data.data(); }
+    const T* data() const { return m_data.data(); }
+
+    uint width()  const { return m_width; }
+    uint height() const { return m_height; }
+    uint size()   const { return m_data.size(); }   // total elements
+
+    T& operator()(uint x, uint y) { 
+        return m_data[y * m_width + x]; 
+    }
+    
+    const T& operator()(uint x, uint y) const { 
+        return m_data[y * m_width + x]; 
+    }
+
+private:
+    std::vector<T> m_data;
+    uint m_width = 0;
+    uint m_height = 0;
+};
+
+*/
--- engine/src/Buffer.cpp
+++ engine/src/Buffer.cpp
@@ -63,3 +63,44 @@ template struct Buffer<double>;
 template struct Buffer<int>;
 template struct Buffer<uchar>;
 template struct Buffer<uint>;
+
+/*
+template<typename T>
+Buffer<T>::Buffer(uint width, uint height) 
+    : m_width(width)
+    , m_height(height)
+    , m_data(width * height)
+{
+}
+
+template<typename T>
+void Buffer<T>::init(T default_value) {
+    if (m_data.empty()) {
+        m_data.resize(m_width * m_height, default_value);
+    }
+}
+
+template<typename T>
+void Buffer<T>::reset(T value) {
+    std::fill(m_data.begin(), m_data.end(), value);
+}
+
+template<typename T>
+T Buffer<T>::getMax() const {
+    if (m_data.empty()) return T{};
+    return *std::max_element(m_data.begin(), m_data.end());
+}
+
+template<typename T>
+T Buffer<T>::getMin() const {
+    if (m_data.empty()) return T{};
+    return *std::min_element(m_data.begin(), m_data.end());
+}
+
+// Explicit instantiations
+template class Buffer<float>;
+template class Buffer<double>;
+template class Buffer<int>;
+template class Buffer<unsigned char>;
+template class Buffer<unsigned int>;
+*/
